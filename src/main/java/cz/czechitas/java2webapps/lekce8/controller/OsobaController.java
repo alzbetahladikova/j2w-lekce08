df4cdb5fe4e55b24ab7f69c91f6b2c61a2380c0d
alzbetahladikova <beta.strelbova@gmail.com>
2023-11-14 19:50:21 +0100

    hotové cviko

--- src/main/java/cz/czechitas/java2webapps/lekce8/controller/OsobaController.java
+++ src/main/java/cz/czechitas/java2webapps/lekce8/controller/OsobaController.java
@@ -1,7 +1,9 @@
 package cz.czechitas.java2webapps.lekce8.controller;
 
 import cz.czechitas.java2webapps.lekce8.entity.Osoba;
+import cz.czechitas.java2webapps.lekce8.repository.OsobaRepository;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
@@ -15,9 +17,15 @@ import java.util.List;
 @Controller
 public class OsobaController {
 
+  private final OsobaRepository osobaRepository;
+  @Autowired
+  public OsobaController(OsobaRepository osobaRepository) {
+    this.osobaRepository = osobaRepository;
+  }
   private final List<Osoba> seznamOsob = List.of(
           new Osoba(1L, "Božena", "Němcová", LocalDate.of(1820, 2, 4), "Vídeň", null, null)
   );
+
 
   @InitBinder
   public void nullStringBinding(WebDataBinder binder) {
@@ -27,9 +35,8 @@ public class OsobaController {
 
   @GetMapping("/")
   public ModelAndView seznam() {
-    //TODO načíst seznam osob
     return new ModelAndView("seznam")
-            .addObject("osoby", seznamOsob);
+            .addObject("osoby", osobaRepository.findAll());
   }
 
   @GetMapping("/novy")
@@ -43,15 +50,17 @@ public class OsobaController {
     if (bindingResult.hasErrors()) {
       return "detail";
     }
-    //TODO uložit údaj o nové osobě
+    osobaRepository.save(osoba);
+
+    //TODO uložit údaj o nové osobě - funguje
     return "redirect:/";
   }
 
   @GetMapping("/{id:[0-9]+}")
   public ModelAndView detail(@PathVariable long id) {
-    //TODO načíst údaj o osobě
+    //TODO načíst údaj o osobě - funguje
     return new ModelAndView("detail")
-            .addObject("osoba", seznamOsob.get(0));
+            .addObject("osoba", osobaRepository.findById(id).get());
   }
 
   @PostMapping("/{id:[0-9]+}")
@@ -59,13 +68,17 @@ public class OsobaController {
     if (bindingResult.hasErrors()) {
       return "detail";
     }
-    //TODO uložit údaj o osobě
+    osobaRepository.save(osoba);
+    //TODO uložit údaj o osobě - funugje
+
     return "redirect:/";
   }
 
   @PostMapping(value = "/{id:[0-9]+}", params = "akce=smazat")
   public String smazat(@PathVariable long id) {
-    //TODO smazat údaj o osobě
+    osobaRepository.deleteById(id);
+
+    //TODO smazat údaj o osobě - funguje
     return "redirect:/";
   }
 
